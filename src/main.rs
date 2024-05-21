f55b32c80305120bd5eba78ebe7870b7ae6f1409
martabettuz <m.bettini00@gmail.com>
2024-05-21 12:24:09 +0200

    First Test of using a graph with 3 nodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use petgraph::graph::Graph;
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    let mut graph = Graph::<&str, &str>::new();
+
+    let a = graph.add_node("A");
+    let b = graph.add_node("B");
+    let c = graph.add_node("C");
+
+    graph.add_edge(a, b, "A to B");
+    graph.add_edge(b, c, "B to C");
+    graph.add_edge(c, a, "C to A");
+
+    println!("{:?}", graph);
 }
